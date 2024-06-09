8ca3641ca40958186b828a0d29a7fbd690368fee
João Paulo Dias <joaopdiasventura@gmail.com>
2024-06-09 19:53:54 -0300

    feat: adicionando comentarios no código

--- snake/draws.js
+++ snake/draws.js
@@ -1,5 +1,14 @@
-import { canvas, ctx, division, WidthSquare, HeigthSquare, snake, fruit } from "./script.js";
+import {
+  canvas,
+  ctx,
+  division,
+  WidthSquare,
+  HeigthSquare,
+  snake,
+  fruit,
+} from "./script.js";
 
+// funções para o desenho do jogo
 export function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawGrades();
@@ -10,7 +19,7 @@ export function draw() {
 }
 
 function drawGrades() {
-  ctx.fillStyle = "#aaa"
+  ctx.fillStyle = "#aaa";
   for (let i = 1; i < division; i++) {
     ctx.fillRect(0, i * WidthSquare, canvas.width, 0.8);
     ctx.fillRect(i * HeigthSquare, 0, 0.8, canvas.height);
@@ -22,7 +31,7 @@ function drawSquare(x, y) {
 }
 
 function drawSnake() {
-  ctx.fillStyle = "#0f0"
+  ctx.fillStyle = "#0f0";
   for (let i = 0; i < snake.length; i++) {
     const { x, y } = snake[i];
     drawSquare(x, y);
--- snake/script.js
+++ snake/script.js
@@ -1,29 +1,44 @@
 import { draw } from "./draws.js";
 
+// recebendo o canvas e o contexto
 export const canvas = document.getElementById("game");
 export const ctx = canvas.getContext("2d");
 
-export const division = 20
+// definindo as medidas do campo
+export const division = 20;
 export const WidthSquare = canvas.width / division;
 export const HeigthSquare = canvas.height / division;
 
-export let snake = [{ x: WidthSquare * Math.floor(division/2), y: HeigthSquare * Math.floor(division/2) }];
+// criando a cobra e a fruta no meio do comapo
+export let snake = [
+  {
+    x: WidthSquare * Math.floor(division / 2),
+    y: HeigthSquare * Math.floor(division / 2),
+  },
+];
 
-export const fruit = { x: WidthSquare * Math.floor(division/2), y: HeigthSquare };
+export const fruit = {
+  x: WidthSquare * Math.floor(division / 2),
+  y: HeigthSquare,
+};
 
+// definindo as direções
 let direction = "";
 let lastDirection = "";
 
 draw();
 
+// chamando a função game a cada 100 milisegundos
 setInterval(game, 100);
 
+// criando a função game
 function game() {
   moveSnake();
   hit();
   eat();
 }
 
+// funções para mover a cobra
 function changeY(y) {
   const newHead = { x: snake[0].x, y: (snake[0].y += y) };
   snake.unshift(newHead);
@@ -63,33 +78,34 @@ function moveSnake() {
   }
 }
 
+// função para adicionar uma parte na cobra
 function eat() {
   if (snake[0].x == fruit.x && snake[0].y == fruit.y) {
     switch (direction) {
       case "up":
         snake.push({
-          y: (snake[snake.length - 1].y),
+          y: snake[snake.length - 1].y,
           x: snake[snake.length - 1].x,
         });
         break;
 
       case "down":
         snake.push({
-          y: (snake[snake.length - 1].y),
+          y: snake[snake.length - 1].y,
           x: snake[snake.length - 1].x,
         });
         break;
 
       case "left":
         snake.push({
-          x: (snake[snake.length - 1].x),
+          x: snake[snake.length - 1].x,
           y: snake[snake.length - 1].y,
         });
         break;
 
       case "right":
         snake.push({
-          x: (snake[snake.length - 1].x),
+          x: snake[snake.length - 1].x,
           y: snake[snake.length - 1].y,
         });
         break;
@@ -101,23 +117,25 @@ function eat() {
   }
 }
 
+// função para impedir a cobra de sair do campo
 function hit() {
-    for (let i = 0; i < snake.length; i++) {
-        if (snake[i].x > canvas.width - WidthSquare) {
-            snake[i].x = 0;
-        }
-        if (snake[i].x < 0) {
-            snake[i].x = canvas.width;
-        }
-        if (snake[i].y > canvas.height - HeigthSquare) {
-            snake[i].y = 0;
-        }
-        if (snake[i].y < 0) {
-            snake[i].y = canvas.height;
-        }
+  for (let i = 0; i < snake.length; i++) {
+    if (snake[i].x > canvas.width - WidthSquare) {
+      snake[i].x = 0;
     }
+    if (snake[i].x < 0) {
+      snake[i].x = canvas.width;
+    }
+    if (snake[i].y > canvas.height - HeigthSquare) {
+      snake[i].y = 0;
+    }
+    if (snake[i].y < 0) {
+      snake[i].y = canvas.height;
+    }
+  }
 }
 
+// função para mudar a posição da cobra
 function changeFruitPosition() {
   const max = division - 1;
   fruit.x = Math.floor(Math.random() * max) * WidthSquare;
@@ -130,6 +148,7 @@ function changeFruitPosition() {
   }
 }
 
+// recebendo os eventos do teclado
 document.addEventListener("keydown", (e) => {
   switch (e.key) {
     case "ArrowUp":
